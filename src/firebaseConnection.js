ea0632ce0240bac6d7d3add9167895fe68ef9f19
user <user@Estevao-Esteves.local>
2026-03-19 21:39:43 +0000

    config de firebase

---
+++ src/firebaseConnection.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAgUktSGTsqFU73maIdvKTdfMu_UpWZbLw",
+  authDomain: "curso-react-e52a0.firebaseapp.com",
+  databaseURL:
+    "https://curso-react-e52a0-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "curso-react-e52a0",
+  storageBucket: "curso-react-e52a0.firebasestorage.app",
+  messagingSenderId: "1065039719649",
+  appId: "1:1065039719649:web:d4fd15ac30cddf15019ba0",
+  measurementId: "G-D9EDKMP3V3",
+};
+
+const firebaseAPP = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseAPP);
+
+export { db };
